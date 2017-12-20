624b200f55a9ced160bf9c9d2927da5dbd1ef29f
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-12-20 11:28:18 +0100

    Added -p and -l option

--- spark-start-sge-cluster.sh
+++ spark-start-sge-cluster.sh
@@ -6,8 +6,9 @@ DIR=.spark
 EXECUTORS=4
 CPU_PER_EXECUTOR=4
 GB_MEM_PER_EXECUTOR=20
+MEMORY_RESOURCE=h_vmem
 
-while getopts "d:e:c:m:" opt; do
+while getopts "d:e:c:m:p:l:" opt; do
   case ${opt} in
     d ) DIR=$OPTARG
       ;;
@@ -17,6 +18,10 @@ while getopts "d:e:c:m:" opt; do
       ;;
     m ) GB_MEM_PER_EXECUTOR=$OPTARG
       ;;
+    p ) PE_ENV=$OPTARG
+      ;;
+    l ) MEMORY_RESOURCE=$OPTARG
+      ;;
     \? ) 
       echo "Usage: $0 [-d] output_dir [-e] executors [-c] cpu_per_executor [-m] gb_mem_per_executor"
       exit 1
@@ -24,10 +29,20 @@ while getopts "d:e:c:m:" opt; do
   esac
 done
 
+if [ -z "$PE_ENV" ]
+then
+      echo "Parallel env is required, please set it with -p"
+      exit 1
+fi
+
+set -eu
+
 echo "Dir: $DIR"
 echo "Executors: $EXECUTORS"
 echo "Cpu per executor: $CPU_PER_EXECUTOR"
 echo "Gb mem per executor: $GB_MEM_PER_EXECUTOR"
+echo "Parallel env: $PE_ENV"
+echo "Memory resource: $MEMORY_RESOURCE"
 
 WORK_DIR=$DIR/work
 LOG_DIR=$DIR/log
@@ -55,7 +70,7 @@ echo "Spark master: $SPARK_MASTER"
 for i in $(seq 1 $EXECUTORS)
 do
 qrsh -N spark-worker -now no \
--pe BWA $CPU_PER_EXECUTOR -l h_vmem=`echo "$GB_MEM_PER_CPU * 1.2" | bc`G \
+-pe $PE_ENV $CPU_PER_EXECUTOR -l ${MEMORY_RESOURCE}=`echo "$GB_MEM_PER_CPU * 1.2" | bc`G \
 ssh localhost -t -t `which spark-class` \
 org.apache.spark.deploy.worker.Worker $SPARK_MASTER \
 -c $CPU_PER_EXECUTOR \
